b40cf54a3ab4a1cf025c07da5f0e84acb1835f36
Inkorest <1394689456@qq.com>
2026-05-25 16:32:38 +0800

    remove(register): 移除多余声明

--- include/register.h
+++ include/register.h
@@ -1,15 +1,6 @@
 #ifndef REGISTER_H
 #define REGISTER_H
-
-typedef struct User
-{
-    char username[20];
-    char password[20];
-    char phone[15];
-    struct User *next;
-} User;
-
-User *g_user_head;
+#include "user.h"
 
 void user_register(User *head, User *target);
 
